7755eb50c828c9057df3e157427ea0333e443a00
Adam Lopez <Adamf04@gmail.com>
2018-11-24 20:03:13 -0500

    Testing performance of index page and Wordpress functions and how they display posts and pages programmatically using while loops.

--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -1,10 +1,10 @@
+<!-- when a single post is clicked, it will show this instead of index.php -->
 <?php 
 // keep looping while we still have posts to loop through
 while (have_posts()) {
 	the_post(); ?>
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<h2><?php the_title(); ?></h2>
 	<?php the_content(); ?>
-	<hr>
 <?php }
 
  ?>
